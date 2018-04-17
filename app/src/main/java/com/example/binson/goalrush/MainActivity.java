8f98b075fdb796ebcd830a78ccf90b29fc349146
binson <binson1989@gmail.com>
2018-04-18 00:02:49 +0530

    use tablayout that loads different fragments

--- app/src/main/java/com/example/binson/goalrush/MainActivity.java
+++ app/src/main/java/com/example/binson/goalrush/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.binson.goalrush;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    MyPagerAdapter myPagerAdapter;
+    ViewPager viewPager;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        viewPager = findViewById(R.id.pager);
+        viewPager.setAdapter(myPagerAdapter);
+        tabLayout = findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public static class MyPagerAdapter extends FragmentPagerAdapter {
+        private static final int NUM_ITEMS = 3;
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return NewsFragment.newInstance(1, "Hello World");
+                case 1:
+                    return NewsFragment.newInstance(2, "Hello Universe");
+                case 2:
+                    return NewsFragment.newInstance(3, "Hello Infinity");
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Page " + position;
+        }
     }
 }
---
+++ app/src/main/java/com/example/binson/goalrush/NewsFragment.java
@@ -0,0 +1,107 @@
+package com.example.binson.goalrush;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link NewsFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link NewsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NewsFragment extends Fragment {
+    private static final String ARG_PAGE = "page";
+    private static final String ARG_TITLE = "title";
+
+    private int page;
+    private String title;
+
+    private OnFragmentInteractionListener mListener;
+
+    public NewsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param page Parameter 1.
+     * @param title Parameter 2.
+     * @return A new instance of fragment NewsFragment.
+     */
+    public static NewsFragment newInstance(int page, String title) {
+        NewsFragment fragment = new NewsFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        args.putString(ARG_TITLE, title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            page = getArguments().getInt(ARG_PAGE);
+            title = getArguments().getString(ARG_TITLE);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_news, container, false);
+        TextView content = view.findViewById(R.id.tv_content);
+        content.setText("Page " + page + ": " + title);
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
